02b371de06b978cb5215ac5615aa5b79e852c6ec
YASH-SAWARKAR <nitintaronejnt@gmail.com>
2025-07-03 01:02:50 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+// import express from "express";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import dotenv from "dotenv";
 
-const app = express()
+// const app = express();
 
-app.use(cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials:true
-}))
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// );
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true , limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-export { app }
+// app.use(express.json({ limit: "16kb" }));
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+// app.use(express.static("public"));
+// app.use(cookieParser());
+
+// // Routes import
+
+// import userRouter from "./routes/user.routes.js";
+
+// // route declaration
+// app.use("/api/v1/users", userRouter);
+
+// //;ocalhost:8000/api/v1/users/register
+// export { app };
+
+import express from "express";
+import userRouter from "./routes/user.routes.js";
+import cors from "cors";
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/users", userRouter);
